2630ce3bb51fe9e300994e24028e5dd8e941fb24
= <nglomski@gmail.com>
2018-01-02 14:47:15 -0600

    Generated all the components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AllPostsComponent,
+    FollowingComponent,
+    FavoritesComponent,
+    MyPostsComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
