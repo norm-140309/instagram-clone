50eca338e595bb5761801a6d3156e898db365b04
= <nglomski@gmail.com>
2018-01-03 09:00:39 -0600

    Added firebase to the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+
+  ngOnInit() {
+    const config = {
+      apiKey: "AIzaSyArMZvQdDCp5yEOQwg2oe4ep5D6XXmpBVs",
+      authDomain: "instaclone-554c3.firebaseapp.com",
+      databaseURL: "https://instaclone-554c3.firebaseio.com",
+      projectId: "instaclone-554c3",
+      storageBucket: "instaclone-554c3.appspot.com",
+      messagingSenderId: "334003957971"
+    };
+    firebase.initializeApp(config);
+  }
 }
