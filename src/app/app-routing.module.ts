1c60fa6f39c81801f7b95b3b1a34ca54e906e914
Norm Glomski <nglomski@gmail.com>
2018-01-02 20:27:53 -0600

    Added routing to the app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
-import { Routes } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AllPostsComponent } from "./all-posts/all-posts.component";
 import { FollowingComponent } from "./following/following.component";
 import { FavoritesComponent } from "./favorites/favorites.component";
 import { MyPostsComponent } from "./my-posts/my-posts.component";
+import { NgModule } from "@angular/core";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "allposts", component: AllPostsComponent },
   { path: "following", component: FollowingComponent },
   { path: "favorites", component: FavoritesComponent },
-  { path: "my-posts", component: MyPostsComponent },
+  { path: "myposts", component: MyPostsComponent },
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AllPostsComponent } from './all-posts/all-posts.component';
-import { FollowingComponent } from './following/following.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { MyPostsComponent } from './my-posts/my-posts.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { HomeComponent } from './home/home.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { AllPostsComponent } from "./all-posts/all-posts.component";
+import { FollowingComponent } from "./following/following.component";
+import { FavoritesComponent } from "./favorites/favorites.component";
+import { MyPostsComponent } from "./my-posts/my-posts.component";
+import { SigninComponent } from "./auth/signin/signin.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { HomeComponent } from "./home/home.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 
 @NgModule({
@@ -26,7 +25,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
