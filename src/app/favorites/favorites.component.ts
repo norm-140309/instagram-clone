c74c6152c2c0745feef718f2001437348f2f983a
Norm Glomski <nglomski@gmail.com>
2018-01-07 16:04:46 -0600

    Wired up the favorites page.

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as _ from "lodash";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-favorites',
-  templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  selector: "app-favorites",
+  templateUrl: "./favorites.component.html",
+  styleUrls: ["./favorites.component.css"]
 })
 export class FavoritesComponent implements OnInit {
+  favoritesList: any = [];
 
-  constructor() { }
+  constructor() {
+    const uid = firebase.auth().currentUser.uid;
+    const favRef = firebase.database().ref("favorites").child(uid);
+    favRef.once("value")
+          .then(snapshot => {
+            const favoritesObj = snapshot.val();
+            this.favoritesList = _.values(favoritesObj);
+          });
+  }
 
   ngOnInit() {
   }
