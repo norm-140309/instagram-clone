e3850296f9f136fdcd4870f4a6bfd015f04a4e31
Norm Glomski <nglomski@gmail.com>
2018-01-07 16:47:03 -0600

    Added usersFollowingMe functionality via firebase cloud functions.

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+
+exports.addToFollowing = functions.database.ref("/follow/{initiatorUid}/{interestedInFollowingUid}")
+  .onCreate( event => {
+    const initiatorUid = event.params.initiatorUid;
+    const interestedInFollowingUid = event.params.interestedInFollowingUid;
+    const rootRef = event.data.ref.root;
+    let FollowingMeRef = rootRef.child("usersFollowingMe/" + interestedInFollowingUid + "/" + initiatorUid);
+    return FollowingMeRef.set(true);
+  });
