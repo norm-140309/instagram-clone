0b0f500b785b33a53f0e34485d6f0a41383af4fc
= <nglomski@gmail.com>
2018-01-08 12:37:37 -0600

    Added Slack notification feature that is triggered whenever a new user signs up.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const SlackWebhook = require("slack-webhook");
+const slack = new SlackWebhook("https://hooks.slack.com/services/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");
 
 exports.addToFollowing = functions.database.ref("/follow/{initiatorUid}/{interestedInFollowingUid}")
   .onCreate( event => {
@@ -8,3 +10,20 @@ exports.addToFollowing = functions.database.ref("/follow/{initiatorUid}/{interes
     let FollowingMeRef = rootRef.child("usersFollowingMe/" + interestedInFollowingUid + "/" + initiatorUid);
     return FollowingMeRef.set(true);
   });
+
+  exports.notifyOfNewUser = functions.database.ref("/users/{userId}")
+    .onCreate( event => {
+      const newUser = event.data.val();
+      const msg = `
+Date: ${newUser.registrationDate} 
+Username: ${newUser.name} 
+Email: ${newUser.email} 
+UID: ${newUser.uid} 
+      `;
+      return slack.send( msg )
+        .then(function (res) {
+          console.log({ status: "success", msg: "Slack message sent successfully." }); 
+        }).catch(function (err) {
+          console.log({ status: "error", msg: err }); 
+        });
+    });
